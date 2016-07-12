a96f0ab81ff9d0f5e1724754f6706f1b5003f1e4
camilo9020 <camilo9020@gmail.com>
2016-07-12 12:33:03 -0500

    initial commit

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,39 @@
+class PinsController < ApplicationController
+
+	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
+	def index
+		
+	end
+
+	def show
+		
+	end
+
+	def new
+		@pin = Pin.new
+	end
+
+	def create 
+		@pin = Pin.new(pin_params)
+		if @pin.save
+			flash[:success]= 'Sucessfully created new Pin'
+			redirect_to @pin
+		else
+			render 'new'	
+		end	
+	end
+
+
+	private
+
+	def pin_params
+		params.require(:pin).permit(:title, :description)		
+	end
+
+	def find_pin
+		@pin = Pin.find(params[:id])		
+	end
+
+
+end
